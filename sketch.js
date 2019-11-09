263f2b3948f1e9dbb6dc91f48da570dddbc2c24e
NickXitco <nxitco1@jhu.edu>
2019-11-09 12:56:23 -0500

    init

---
+++ sketch.js
@@ -0,0 +1,71 @@
+let osc1;
+let osc2;
+let playing = true;
+
+let maxY = 0;
+let minY = 10000;
+
+let fft;
+
+let SAMPLE_RATE = 2 ** 14;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(250, 170, 0);
+
+    /*
+    osc1 = new p5.Oscillator();
+    osc1.setType('sine');
+    osc1.freq(440);
+    osc1.amp(0);
+    osc1.start();
+     */
+
+    osc2 = new p5.Oscillator();
+    osc2.setType('sawtooth');
+    osc2.freq(220);
+    osc2.amp(0);
+    osc2.start();
+
+    fft = new p5.FFT(1, SAMPLE_RATE / 1024);
+
+    slider1 = createSlider(20, 1760, 440,  0);
+    slider2 = createSlider(20, 1760, 220,  0);
+    slider1.position(50, 10);
+    slider2.position(50, 50);
+}
+
+function draw() {
+    //osc1.freq(slider1.value());
+    osc2.freq(slider2.value());
+
+    if(playing) {
+        background(250, 170, 0);
+        let waveform = fft.waveform(SAMPLE_RATE, "precise");
+
+        noFill();
+        beginShape();
+        stroke(255,255,255);
+        strokeWeight(3);
+        for (var i = 0; i< waveform.length; i++){
+            let x = map(i, 0, waveform.length, width/2 - 400, width/2 + 400);
+            let y = map( waveform[i], -1, 1, 0, height);
+            vertex(x, y);
+        }
+        endShape();
+
+        //osc1.amp(0.1, 1);
+        osc2.amp(0.1, 1);
+    }
+
+
+    fill(255);
+    strokeWeight(0);
+    textFont('futura-pt', 50);
+    textAlign(CENTER);
+    text('this is a sine wave', windowWidth / 2, (windowHeight / 2)+200);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
